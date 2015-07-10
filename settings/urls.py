e092d860007b00618f9843fa36377bd6c2eca50f
Tao <taoluo@combitel.com.au>
2015-07-10 16:37:18 +1000

    add patch for static files

--- settings/urls.py
+++ settings/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import os
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
@@ -12,3 +13,5 @@ urlpatterns = patterns('',
     # url(r'^%s/(?P<path>.*)$' % ID_PHOTO_FOLDER, 'django.views.static.serve', {'document_root': os.path.join(BASE_DIR, ID_PHOTO_FOLDER).replace('\\', '/'), 'show_indexes': False}),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )
+
+urlpatterns += staticfiles_urlpatterns()
