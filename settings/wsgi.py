ee0c353e49983c4677efcddacb8a48879d721b91
Tao <taoluo@combitel.com.au>
2015-07-10 16:37:02 +1000

    add collectstatic command

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecosway.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
