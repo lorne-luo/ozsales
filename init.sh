6ecdbe8058ceee351e6130f864be338e2441e5d2
Tao <taoluo@combitel.com.au>
2015-07-15 16:39:04 +1000

    refine init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
-python manage.py syncdb && python manage.py migrate && python manage.py init_groups && python manage.py init_data && python manage.py collectstatic
+mysqladmin drop ozsales -f -uroot -proot &&
+mysqladmin create ozsales -uroot -proot &&
+python manage.py makemigrations && 
+python manage.py syncdb --noinput && 
+python manage.py migrate && 
+python manage.py init_groups && 
+python manage.py init_data &&
+python manage.py loaddata deploy/auth.json 
+#python manage.py collectstatic
 # && python manage.py createsuperuser
