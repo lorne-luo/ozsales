de35f54cca36b7a4cfb4106aca6ab2eca0c88073
Tao <taoluo@combitel.com.au>
2015-07-15 17:13:31 +1000

    add test data

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ python manage.py syncdb --noinput &&
 python manage.py migrate && 
 python manage.py init_groups && 
 python manage.py init_data &&
-python manage.py loaddata deploy/auth.json 
+python manage.py loaddata deploy/auth.json &&
+python manage.py loaddata deploy/test_data.json
 #python manage.py collectstatic
 # && python manage.py createsuperuser
