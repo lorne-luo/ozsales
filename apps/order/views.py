16bbc95a657d901c3117483371906fe916e8b3a7
Tao <www@luotao.net>
2015-07-15 23:46:05 +1000

    add view for change order status

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render_to_response, render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.forms.models import modelformset_factory, inlineformset_factory
+from django.forms import ModelForm
+from django.views.generic import CreateView, UpdateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
+from django.contrib.auth.models import User, Group, AnonymousUser
+from django.db.models import Q
+from django.contrib.auth import logout
+from django.http import Http404, HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
+from django.utils.http import urlquote, urlunquote
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from models import Order
 
-# Create your views here.
+
+def change_order_status(request, order_id, status_str):
+    order = get_object_or_404(Order, pk=order_id)
+    if order:
+        order.status = status_str
+        order.save()
+
+    return HttpResponseRedirect(reverse('admin:%s_%s_changelist' % ('order', 'order')))
--- settings/urls.py
+++ settings/urls.py
@@ -12,6 +12,13 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^%s/(?P<path>.*)$' % ID_PHOTO_FOLDER, 'django.views.static.serve', {'document_root': os.path.join(BASE_DIR, ID_PHOTO_FOLDER).replace('\\', '/'), 'show_indexes': False}),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+
+    url(r'^order/(?P<order_id>\d+)/(?P<status_str>\w+)/$', 'apps.order.views.change_order_status', name="change-order-status"),
+    # url('^order/', 'apps.order.urls'),
+    # url('^customer/', 'customer.urls'),
+    # url('^store/', 'store.urls'),
+    # url('^product/', 'product.urls'),
+    # url('^seller/', 'seller.urls'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
