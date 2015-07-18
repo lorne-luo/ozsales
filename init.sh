2fe024e30b2a0d441f05e3d67161bfd6e3fb4be4
Tao <www@luotao.net>
2015-07-18 20:18:35 +1000

    backup db before drop

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+mkdir -p deploy/backup
+var=`date "+%Y-%m-%d-%H-%M-%S"`
+mysqldump -uroot -proot ozsales > deploy/backup/ozsales_$var.sql
+echo "backup current db to deploy/backup/ozsales_$var.sql"
+
 mysqladmin drop ozsales -f -uroot -proot &&
 mysqladmin create ozsales -uroot -proot &&
 python manage.py makemigrations && 
