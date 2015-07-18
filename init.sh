7fe9e9f6220fd20d97e4960e06de74049dfc9432
Tao <www@luotao.net>
2015-07-18 21:45:47 +1000

    modify  init_product data

--- init.sh
+++ init.sh
@@ -11,7 +11,10 @@ python manage.py migrate &&
 python manage.py init_groups && 
 python manage.py init_data &&
 python manage.py loaddata deploy/init_user.json &&
-python manage.py loaddata deploy/init_product.json
+python manage.py loaddata deploy/init_product.json &&
+python manage.py loaddata deploy/init_tag.json &&
+python manage.py loaddata deploy/init_express.json
+
 
 #python manage.py loaddata deploy/test_data.json
 #python manage.py collectstatic
