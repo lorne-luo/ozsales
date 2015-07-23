66a6f11cf60959771d59ff3eb444a1f24d144e12
Tao <www@luotao.net>
2015-07-23 23:13:18 +1000

    add static folder

---
+++ static/style/js/main.js
@@ -0,0 +1,29 @@
+
+jQuery(document).ready(function($) {
+    $("select[multiple]").css("width", '300px').attr('data-placeholder', '多选').chosen();
+    jQuery("#id_customer").attr('style','width:150px').chosen();
+    jQuery("#id_products-0-product").chosen();
+    jQuery("#id_products-1-product").chosen();
+    jQuery("#id_products-2-product").chosen();
+    jQuery("#id_products-3-product").chosen();
+    jQuery("#id_products-4-product").chosen();
+    jQuery("#id_products-5-product").chosen();
+    jQuery("#id_products-6-product").chosen();
+    jQuery("#id_products-7-product").chosen();
+    jQuery("#id_products-8-product").chosen();
+    jQuery("#id_products-9-product").chosen();
+    jQuery("#id_products-10-product").chosen();
+    jQuery("#id_products-0-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-1-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-2-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-3-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-4-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-5-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-6-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-7-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-8-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-9-store").attr('style','width:100px').chosen();
+    jQuery("#id_products-10-store").attr('style','width:100px').chosen();
+
+
+});
