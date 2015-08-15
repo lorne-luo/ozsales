eac25b5606b324c0d462b8e651549c8fb8e1b3f5
Tao <www@luotao.net>
2015-08-16 00:57:43 +1000

    remove multiple select chosen

--- static/style/js/main.js
+++ static/style/js/main.js
@@ -1,13 +1,12 @@
 
 jQuery(document).ready(function($) {
-    $("select[multiple]").css("width", '300px').attr('data-placeholder', '多选').chosen();
     $("select[id$='customer']").attr('style','width:150px').chosen();
 	$("select[id$='address']").attr('style','width:500px').chosen();
     $("select[id$='product']").attr('style','width:400px').chosen();
     $("select[id$='store']").attr('style','width:120px').chosen();
     $("select[id$='carrier']").attr('style','width:120px').chosen();
     $("select[id$='brand']").attr('style','width:150px').chosen();
-    $("select[id$='category']").attr('style','width:200px').chosen();
+    $("select[id$='category']").attr('style','width:300px').chosen();
     $("select[id$='country']").attr('style','width:120px').chosen();
 
 });
