091a33981cdb02a52a9dec6278ce29e19b07536d
Tao <www@luotao.net>
2015-09-21 10:22:51 +0800

    recover select chosen in inline

--- static/style/js/main.js
+++ static/style/js/main.js
@@ -1,12 +1,12 @@
 
 jQuery(document).ready(function($) {
-    $("select[id$='customer']").attr('style','width:150px').chosen();
-	$("select[id$='address']").attr('style','width:500px').chosen();
-    $("select[id$='product']").attr('style','width:400px').chosen();
-    $("select[id$='store']").attr('style','width:120px').chosen();
-    $("select[id$='carrier']").attr('style','width:120px').chosen();
-    $("select[id$='brand']").attr('style','width:150px').chosen();
-    $("select[id$='category']").attr('style','width:300px').chosen();
-    $("select[id$='country']").attr('style','width:120px').chosen();
+    $("select[id$='customer']").not(".empty-form select").attr('style','width:150px').chosen();
+	$("select[id$='address']").not(".empty-form select").attr('style','width:500px').chosen();
+    $("select[id$='product']").not(".empty-form select").attr('style','width:400px').chosen();
+    $("select[id$='store']").not(".empty-form select").attr('style','width:120px').chosen();
+    $("select[id$='carrier']").not(".empty-form select").attr('style','width:120px').chosen();
+    $("select[id$='brand']").not(".empty-form select").attr('style','width:150px').chosen();
+    $("select[id$='category']").not(".empty-form select").attr('style','width:300px').chosen();
+    $("select[id$='country']").not(".empty-form select").attr('style','width:120px').chosen();
 
 });
