b8b1bebef8a3d12de94514ef1559033a164ec10b
Tao <www@luotao.net>
2015-09-25 13:25:59 +0800

    adjust task schedule,change expressorder list ordering

--- apps/express/admin.py
+++ apps/express/admin.py
@@ -7,7 +7,7 @@ admin.site.register(ExpressCarrier)
 
 class ExpressOrderAdmin(admin.ModelAdmin):
     list_display = ('carrier', 'get_tracking_link', 'get_order_link', 'address', 'fee', 'weight', 'create_time')
-    ordering = ['create_time']
+    ordering = ['-create_time']
     list_display_links = ['address', 'carrier']
 admin.site.register(ExpressOrder, ExpressOrderAdmin)
 
