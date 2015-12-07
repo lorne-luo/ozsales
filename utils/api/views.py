cc8e9997e75608e002326dad5cc9cea12e1f169e
Tao <taoluo@combitel.com.au>
2015-12-07 15:22:44 +1100

    update ContentTypeObjectView

--- utils/api/views.py
+++ utils/api/views.py
@@ -5,6 +5,7 @@ import logging
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from guardian.shortcuts import get_objects_for_user
 from rest_framework import generics, viewsets, mixins
 from rest_framework.views import APIView
 from rest_framework_extensions.mixins import PaginateByMaxMixin
@@ -40,7 +41,7 @@ class PaginateMaxListAPIView(PaginateByMaxMixin, generics.ListAPIView):
 
 class ContentTypeObjectView(APIView):
     '''
-     Abstract base view for identifying an object in a url like: .../<content_type>/<pk>
+     Abstract base view for identifying an object in a url like: .../<content_type_name>/<pk>
      The view will allow access based on the object of interest and independent
      of the model it actually creates/returns.
      e.g. to read/set Tags of a UserVideo the user needs object permission to
@@ -48,11 +49,11 @@ class ContentTypeObjectView(APIView):
     '''
     def identify_object(self):
         ''' Get object (access permissions are already checked at this point) '''
-        content_type_name = self.kwargs.get('content_type')
+        content_type_name = self.kwargs.get('content_type_name')
         object_id = self.kwargs.get('pk')
         try:
-            self.content_type = ContentType.objects.get(model=content_type_name)
-            return self.content_type.model_class().objects.get(pk=object_id)
+            content_type = ContentType.objects.get(model=content_type_name)
+            return content_type.model_class().objects.get(pk=object_id), content_type
         except ContentType.DoesNotExist:
             log.error("No content type with name '%s' found." % content_type_name)
             raise Http404
@@ -61,3 +62,30 @@ class ContentTypeObjectView(APIView):
                       % (object_id, content_type_name))
             raise Http404
 
+
+class SharedObjectsList(PaginateMaxListAPIView):
+    '''
+     Show list of one model's objects which are shared with current user.
+     Set ?use_groups=False if you want to exclude objects which are not shared
+     with the user itself but only with one of his groups.
+    '''
+    def get_queryset(self):
+        use_groups = self.request.GET.get('use_groups')
+        if use_groups != None:
+            try:
+                # Turn string 'False' or 'True' to boolean
+                use_groups = ast.literal_eval(use_groups)
+            except ValueError:
+                log.info("Invalid parameter for use_groups, use 'True' or 'False'.")
+                use_groups = True
+        else:
+            use_groups = True
+
+        contenttype = ContentType.objects.get_for_model(self.model)
+        view_perm = "%s.view_%s" % (contenttype.app_label, self.model.__name__.lower())
+
+        objects = get_objects_for_user(self.request.user, [view_perm],
+                                       klass=self.model,
+                                       use_groups=use_groups)
+
+        return objects
