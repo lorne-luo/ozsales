f2968ba92dd42bb1e4a7779880e9541ac6112db4
Tao <www@luotao.net>
2016-01-04 08:19:02 +0800

    fix carrier select

--- static/style/js/main.js
+++ static/style/js/main.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 	$("select[id$='address']").not(".empty-form select").attr('style','width:500px').chosen({search_contains: true,disable_search_threshold: 10});
     $("select[id$='product']").not(".empty-form select").attr('style','width:400px').chosen({search_contains: true,disable_search_threshold: 10});
     $("select[id$='store']").not(".empty-form select").attr('style','width:120px').chosen({search_contains: true,disable_search_threshold: 10});
-    $("select[id$='carrier']").not(".empty-form select").attr('style','width:120px').chosen({search_contains: true,disable_search_threshold: 10});
+    $("select[id$='carrier']").not(".empty-form select").attr('style','width:120px').chosen({search_contains: true});
     $("select[id$='brand']").not(".empty-form select").attr('style','width:150px').chosen({search_contains: true,disable_search_threshold: 10});
     $("select[id$='category']").not(".empty-form select").attr('style','width:300px').chosen({search_contains: true,disable_search_threshold: 10});
     $("select[id$='country']").not(".empty-form select").attr('style','width:120px').chosen({search_contains: true,disable_search_threshold: 10});
