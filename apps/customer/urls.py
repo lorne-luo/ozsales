09d182dc8c1dd1151896f3a353b7d1981df9db7d
Tao <taoluo@combitel.com.au>
2016-01-06 14:36:59 +1100

    update js reverse

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('apps.customer.views',
     url(r'^add/$', views.CustomerAddEdit.as_view(), name="customer-add"),
     url(r'^edit/(?P<pk>\d+)/$', views.CustomerAddEdit.as_view(), name="customer-edit"),
 
-    url(r'^customer/list$', login_required(views.CustomerListView.as_view()), name="customer-list-view"),
-    url(r'^customer/add$', login_required(views.CustomerCreateView.as_view()), name="customer-add-view"),
+    url(r'^customer/list/$', login_required(views.CustomerListView.as_view()), name="customer-list-view"),
+    url(r'^customer/add/$', login_required(views.CustomerCreateView.as_view()), name="customer-add-view"),
     url(r'^customer/(?P<pk>\d+)/$', login_required(views.CustomerDetailView.as_view()), name="customer-detail-view"),
     url(r'^customer/(?P<pk>\d+)/edit/$', login_required(views.CustomerUpdateView.as_view()), name="customer-update-view"),
-    url(r'^customer/(?P<pk>\d+)/delete/$', login_required(views.CustomerDeleteView.as_view()), name="customer-delete-view"),
+    url(r'^customer/delete/$', login_required(views.CustomerDeleteView.as_view()), name="customer-delete-view"),
 )
---
+++ js_reverse.sh
@@ -0,0 +1,2 @@
+python manage.py collectstatic_js_reverse
+cp -rf collectstatic/django_js_reverse/js/ static/django_js_reverse/js
