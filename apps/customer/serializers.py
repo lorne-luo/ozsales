dcef622f35fe6d57dcedf8ecb04aa4e1a117cebe
Tao <www@luotao.net>
2016-01-12 00:08:38 +1100

    get link according to permission

--- apps/customer/serializers.py
+++ apps/customer/serializers.py
@@ -5,6 +5,7 @@ from .models import Customer
 
 class CustomerSerializer(serializers.ModelSerializer):
     primary_address = serializers.CharField()
+    name = serializers.SerializerMethodField('get_link')
 
     class Meta:
         model = Customer
@@ -12,3 +13,13 @@ class CustomerSerializer(serializers.ModelSerializer):
         read_only_fields = (
             'id', 'date_joined'
         )
+
+    def get_link(self, obj):
+        request = self.context.get('request', None)
+        change_perm_str = '%s.change_%s' % (self.Meta.model._meta.app_label, self.Meta.model._meta.model_name)
+        view_perm_str = '%s.view_%s' % (self.Meta.model._meta.app_label, self.Meta.model._meta.model_name)
+        if request.user.has_perm(change_perm_str):
+            return obj.get_edit_link()
+        elif request.user.has_perm(view_perm_str):
+            return obj.get_detail_link()
+        return obj.name
