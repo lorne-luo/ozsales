cc07a11d6738c03f59df3fe4018dac274d67972f
Tao <taoluo@combitel.com.au>
2016-01-25 08:47:18 +1100

    refactor product list js

--- apps/product/static/js/product/product_list.js
+++ apps/product/static/js/product/product_list.js
@@ -1,10 +1,10 @@
 var productListPageVue = new CommonListPageVue({
         data:{
-            url:Urls['allowany-product-list']()
+            url:Urls['allowany-product-list'](),
+            create_url_tag:'product-add-view',
+            detail_url_tag:'product-list-view',
+            update_url_tag:'product-edit',
+            delete_url_tag:'product-add'
         }
     }
 );
-productListPageVue.create_url_tag = 'product-add';
-productListPageVue.detail_url_tag = 'product-list-view';
-productListPageVue.update_url_tag = 'product-edit';
-productListPageVue.delete_url_tag = 'product-add';
