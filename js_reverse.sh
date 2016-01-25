770138b5223401db23134a36900fd4c022c35a6d
Tao <www@luotao.net>
2016-01-25 23:28:15 +1100

    update js_reverse

--- js_reverse.sh
+++ js_reverse.sh
@@ -1,2 +1,2 @@
 python manage.py collectstatic_js_reverse
-cp -rf collectstatic/django_js_reverse/js/ static/django_js_reverse/js
+cp -rf collectstatic/django_js_reverse/js/reverse.js static/django_js_reverse/js/reverse.js
