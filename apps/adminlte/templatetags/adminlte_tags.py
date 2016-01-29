918fdeb274dc8fc3e78c4b17d16f3535a3c98c30
Tao <taoluo@combitel.com.au>
2016-01-29 14:52:34 +1100

    fix manytomanyfield bug in tags

--- apps/adminlte/templatetags/adminlte_tags.py
+++ apps/adminlte/templatetags/adminlte_tags.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 from datetime import datetime
+import django
+# from django.db.models.fields.related import ManyRelatedManager
+import django.db.models.fields.related as related
 from django import template
 from django.db.models import ImageField
 from utils.converter import format_datetime
@@ -17,6 +20,12 @@ def render_field_label(obj, field_name):
         for mf in meta_fields:
             if mf.name == field_name:
                 return mf.verbose_name
+
+        many_to_many_fields = obj._meta.many_to_many
+        for mf in many_to_many_fields:
+            if mf.name == field_name:
+                return mf.verbose_name
+
         return u'未知'
     else:
         return u''
@@ -31,12 +40,21 @@ def render_field_value(obj, a):
             value = '-'
         else:
             if hasattr(value, 'field'):
-                if isinstance(value.field, ImageField) and value:
-                    value = "<img style=\"height:90px\" src='%s' />" % value.url
-                else:
-                    value = '<img style=\"height:90px\" src="/static/img/no_image.jpg" alt="没有图片" />'
+                if isinstance(value.field, ImageField):
+                    if value:
+                        value = "<img style=\"height:90px\" src='%s' />" % value.url
+                    else:
+                        value = '<img style=\"height:90px\" src="/static/img/no_image.jpg" alt="没有图片" />'
+
             elif hasattr(obj, 'get_%s_display' % a):
                 value = getattr(obj, 'get_%s_display' % a)()
             elif isinstance(value, datetime):
                 value = format_datetime(value)
+            elif isinstance(value, django.db.models.fields.related.RelatedField):
+                pass
+            elif hasattr(value, 'through'):
+                objects = value.all()
+                ret = ', '.join([unicode(o) for o in objects])
+                return ret
+
     return value
