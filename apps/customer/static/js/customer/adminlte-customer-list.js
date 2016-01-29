9dc436e927c454244caeeda91721853bdb1889fe
Tao <taoluo@combitel.com.au>
2016-01-29 15:53:59 +1100

    default common list js

--- apps/customer/static/js/customer/adminlte-customer-list.js
+++ apps/customer/static/js/customer/adminlte-customer-list.js
@@ -1,5 +1,2 @@
 var customerListPageVue = new CommonListPageVue();
-customerListPageVue.create_url_tag = 'customer-add-view';
-customerListPageVue.detail_url_tag = 'customer-detail-view';
-customerListPageVue.update_url_tag = 'customer-update-view';
-customerListPageVue.delete_url_tag = 'customer-delete-view';
+
