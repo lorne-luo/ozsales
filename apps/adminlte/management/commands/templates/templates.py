1cbad412b15837ba9f879e5b1c8b5cfebef14e43
Tao <taoluo@combitel.com.au>
2016-01-30 14:27:01 +1100

    export all model url into frontend js

--- apps/adminlte/management/commands/templates/templates.py
+++ apps/adminlte/management/commands/templates/templates.py
@@ -10,11 +10,16 @@ LIST_TEMPLATES = '''
 LIST_JS = '''
 var <% model_name %>ListPageVue = new CommonListPageVue({
         data: {
+            add_api_tag: 'api-<% model_name %>-list',
             list_api_tag: 'api-<% model_name %>-list',
+            delete_api_tag: 'api-<% model_name %>-delete',
+            retrieve_api_tag: 'api-<% model_name %>-detail',
+            update_api_tag: 'api-<% model_name %>-detail',
+            destroy_api_tag: 'api-<% model_name %>-detail',
+
             create_url_tag: '<% model_name %>-add',
             detail_url_tag: '<% model_name %>-detail',
             update_url_tag: '<% model_name %>-update',
-            delete_api_tag: '<% model_name %>-delete'
         }
     }
 );
