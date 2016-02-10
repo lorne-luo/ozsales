536a552110e1dd835573d4fc168c927a830ac609
Tao <taoluo@combitel.com.au>
2016-02-10 17:30:24 +1100

    delete unused file

--- core/messageset/urls_api.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from django.conf.urls import url
-from core.messageset import views_api
-
-urlpatterns = [
-    url(r'/sitemail/markall', views_api.sitemail_markall,
-        name='sitemail_markall'),
-]
