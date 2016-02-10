997ab0ec5f59c5285e44d3230fe81ace20fbf912
Tao <taoluo@combitel.com.au>
2016-02-10 17:34:48 +1100

    update messageset static

---
+++ core/messageset/static/js/messageset/notification_list.js
@@ -0,0 +1,16 @@
+
+var notificationListPageVue = new CommonListPageVue({
+        data: {
+            add_api_tag: 'messageset:api-notification-list',
+            list_api_tag: 'messageset:api-notification-list',
+            delete_api_tag: 'messageset:api-notification-delete',
+            retrieve_api_tag: 'messageset:api-notification-detail',
+            update_api_tag: 'messageset:api-notification-detail',
+            destroy_api_tag: 'messageset:api-notification-detail',
+
+            create_url_tag: 'messageset:notification-add',
+            detail_url_tag: 'messageset:notification-detail',
+            update_url_tag: 'messageset:notification-update'
+        }
+    }
+);
---
+++ core/messageset/static/js/messageset/sitemailcontent_list.js
@@ -0,0 +1,16 @@
+
+var sitemailcontentListPageVue = new CommonListPageVue({
+        data: {
+            add_api_tag: 'messageset:api-sitemailcontent-list',
+            list_api_tag: 'messageset:api-sitemailcontent-list',
+            delete_api_tag: 'messageset:api-sitemailcontent-delete',
+            retrieve_api_tag: 'messageset:api-sitemailcontent-detail',
+            update_api_tag: 'messageset:api-sitemailcontent-detail',
+            destroy_api_tag: 'messageset:api-sitemailcontent-detail',
+
+            create_url_tag: 'messageset:sitemailcontent-add',
+            detail_url_tag: 'messageset:sitemailcontent-detail',
+            update_url_tag: 'messageset:sitemailcontent-update'
+        }
+    }
+);
