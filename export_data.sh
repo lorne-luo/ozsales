3da9d93d28dc04f013a4904ada82a303f6d95c8a
Tao <www@luotao.net>
2016-02-17 12:06:43 +0800

    delete used sh

--- export_data.sh
+++
@@ -1,1 +0,0 @@
-python manage.py dumpdata --indent 2 customer store product express order >static/db.json
--- export_init.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p deploy/export
-
-
-python manage.py dumpdata --indent 2 customer.interesttag> deploy/export/init_tag.json 
-python manage.py dumpdata --indent 2 express.expresscarrier> deploy/export/init_express.json
