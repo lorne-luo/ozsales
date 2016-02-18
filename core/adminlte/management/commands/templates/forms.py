c6bdb6ea23bb15b442d8c1ba4c2ecf74d8de2ebd
Tao <taoluo@combitel.com.au>
2016-02-18 23:41:59 +1100

    extend django.forms.ModelForm to remove holddown msg

--- core/adminlte/management/commands/templates/forms.py
+++ core/adminlte/management/commands/templates/forms.py
@@ -1,22 +1,22 @@
 FORMS_HEADER = '''# coding=utf-8
-from django import forms
+from core.libs.forms import ModelForm  # extend from django.forms.ModelForm
 from models import <% ALL_MODELS %>
 
 '''
 FORMS_MODEL_TEMPLATE = '''
-class <% MODEL_NAME %>AddForm(forms.ModelForm):
+class <% MODEL_NAME %>AddForm(ModelForm):
     class Meta:
         model = <% MODEL_NAME %>
         fields = <% fields %>
 
 
-class <% MODEL_NAME %>UpdateForm(forms.ModelForm):
+class <% MODEL_NAME %>UpdateForm(ModelForm):
     class Meta:
         model = <% MODEL_NAME %>
         fields = <% fields %>
 
 
-class <% MODEL_NAME %>DetailForm(forms.ModelForm):
+class <% MODEL_NAME %>DetailForm(ModelForm):
     class Meta:
         model = <% MODEL_NAME %>
         fields = <% fields %>
