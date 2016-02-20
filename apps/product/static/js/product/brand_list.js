d599f3a5b91b6a1344efff703a6af240ed7e4d84
Tao <taoluo@combitel.com.au>
2016-02-21 09:51:05 +1100

    add new template files

---
+++ apps/product/static/js/product/brand_list.js
@@ -0,0 +1,16 @@
+
+var brandListPageVue = new CommonListPageVue({
+        data: {
+            add_api_tag: 'product:api-brand-list',
+            list_api_tag: 'product:api-brand-list',
+            delete_api_tag: 'product:api-brand-delete',
+            retrieve_api_tag: 'product:api-brand-detail',
+            update_api_tag: 'product:api-brand-detail',
+            destroy_api_tag: 'product:api-brand-detail',
+
+            create_url_tag: 'product:brand-add',
+            detail_url_tag: 'product:brand-detail',
+            update_url_tag: 'product:brand-update'
+        }
+    }
+);
