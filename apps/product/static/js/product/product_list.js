c89e08b24efd041a77e4c9b85deb9a1c75fbb119
Tao <taoluo@combitel.com.au>
2016-02-21 00:56:36 +1100

    update list api url

--- apps/product/static/js/product/product_list.js
+++ apps/product/static/js/product/product_list.js
@@ -1,6 +1,6 @@
 var productListPageVue = new CommonListPageVue({
         data: {
-            list_api_tag: 'allowany-product-list',
+            list_api_tag: 'product:allowany-product-list',
             create_url_tag: 'product-add-view',
             detail_url_tag: 'product-detail-view',
             update_url_tag: 'product-update-view',
