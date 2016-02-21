70b11ff5d796b16e4e329b8ba5bcfbeb8797f8cb
Tao <taoluo@combitel.com.au>
2016-02-21 11:27:20 +1100

    use new api in public product

--- apps/product/static/js/product/product_list.js
+++ apps/product/static/js/product/product_list.js
@@ -1,10 +1,10 @@
 var productListPageVue = new CommonListPageVue({
         data: {
-            list_api_tag: 'product:allowany-product-list',
-            create_url_tag: 'product-add-view',
-            detail_url_tag: 'product-detail-view',
-            update_url_tag: 'product-update-view',
-            delete_url_tag: 'product-add'
+            list_api_tag: 'product:api-product-list',
+            create_url_tag: 'product:product-add-view',
+            detail_url_tag: 'product:product-detail-view',
+            update_url_tag: 'product:product-update-view',
+            delete_url_tag: 'product:product-add'
         }
     }
 );
