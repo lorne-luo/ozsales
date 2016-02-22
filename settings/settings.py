2d42ff1bc3c206c65ae1081c0d75a1d2a65aa946
Tao <taoluo@combitel.com.au>
2016-02-22 20:58:52 +1100

    add media in template_context_processors

--- settings/settings.py
+++ settings/settings.py
@@ -153,6 +153,7 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
+    'django.core.context_processors.media',
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
