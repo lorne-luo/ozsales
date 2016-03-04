6471031c643a8d9572a4146b52ab07b98a34f2c1
Tao <taoluo@combitel.com.au>
2016-03-04 14:40:59 +1100

    ongoing and finished tab for order list

--- apps/order/static/js/order/order_list.js
+++ apps/order/static/js/order/order_list.js
@@ -1,5 +1,65 @@
+var OrderListPageVue = CommonListPageVue.extend({
+    ready: function () {
+        if(this.appName && this.modelName){
+            this.loadOngoingData({});
+            this.loadFinishedData({});
+        }
+    },
+    methods: {
+        loadOngoingData: function (data) {
+            var self = this;
+            var url;
+            if (self.list_api_tag)
+                url = Urls[self.list_api_tag]();
+            else
+                url = $.AdminLTE.getApiUrl(self.appName, self.modelName);
 
-var orderListPageVue = new CommonListPageVue({
+            var ongoing_url=url+'?status_in=CREATED,SHIPPING,DELIVERED';
+            $.AdminLTE.apiGet(
+                ongoing_url,
+                data,
+                function (resp) {
+                    self.ongoing_items = resp.results;
+                    self.ongoing_count = resp.count;
+                    self.ongoing_perPage = resp.per_page;
+                    self.ongoing_totalPage = resp.total_page;
+                    self.ongoing_currentPage = resp.current_page;
+                }
+            );
+        },
+        loadFinishedData: function (data) {
+            var self = this;
+            var url;
+            if (self.list_api_tag)
+                url = Urls[self.list_api_tag]();
+            else
+                url = $.AdminLTE.getApiUrl(self.appName, self.modelName);
+
+            var finished_url=url+'?ordering=-id&status=FINISHED';
+            $.AdminLTE.apiGet(
+                finished_url,
+                data,
+                function (resp) {
+                    self.finished_items = resp.results;
+                    self.finished_count = resp.count;
+                    self.finished_perPage = resp.per_page;
+                    self.finished_totalPage = resp.total_page;
+                    self.finished_currentPage = resp.current_page;
+                }
+            );
+        },
+        page_ongoing: function () {
+            var num = $(event.target).attr('page');
+            this.loadOngoingData({'page': num});
+        },
+        page_finished: function () {
+            var num = $(event.target).attr('page');
+            this.loadFinishedData({'page': num});
+        }
+    }
+});
+
+var orderListPageVue = new OrderListPageVue({
         data: {
             add_api_tag: 'order:api-order-list',
             list_api_tag: 'order:api-order-list',
@@ -10,7 +70,17 @@ var orderListPageVue = new CommonListPageVue({
 
             create_url_tag: 'order:order-add',
             detail_url_tag: 'order:order-detail',
-            update_url_tag: 'order:order-update'
+            update_url_tag: 'order:order-update',
+            finished_items: [],
+            ongoing_items: [],
+            finished_currentPage: 1,
+            finished_totalPage: 1,
+            finished_perPage: 10,
+            finished_count: 0,
+            ongoing_currentPage: 1,
+            ongoing_totalPage: 1,
+            ongoing_perPage: 10,
+            ongoing_count: 0
         }
     }
 );
