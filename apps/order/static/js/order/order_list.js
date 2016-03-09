628827c8261a0dda980015192e0039b6075c86e1
Tao <taoluo@combitel.com.au>
2016-03-10 00:23:40 +1100

    fix detail url for order list

--- apps/order/static/js/order/order_list.js
+++ apps/order/static/js/order/order_list.js
@@ -55,7 +55,38 @@ var OrderListPageVue = CommonListPageVue.extend({
         page_finished: function () {
             var num = $(event.target).attr('page');
             this.loadFinishedData({'page': num});
-        }
+        },
+        detail: function (event) {
+            var pk,customer;
+            if ($(event.target).data('pk'))
+                pk = $(event.target).data('pk');
+            else if ($(event.target.parentNode).data('pk'))
+                pk = $(event.target.parentNode).data('pk');
+            else{
+                swal('错误', '无法获取pk', 'error');
+                return;
+            }
+            if ($(event.target).data('customer'))
+                customer = $(event.target).data('customer');
+            else if ($(event.target.parentNode).data('customer'))
+                customer = $(event.target.parentNode).data('customer');
+            else{
+                swal('错误', '无法获取customer', 'error');
+                return;
+            }
+
+            var url;
+            if (this.detail_url_tag){
+                url = Urls[this.detail_url_tag](customer,pk);
+            }else{
+                url = Urls['adminlte:common_detail_page'](
+                    this.appName,
+                    this.modelName,
+                    pk
+                );
+            }
+            window.location.href = url;
+        },
     }
 });
 
