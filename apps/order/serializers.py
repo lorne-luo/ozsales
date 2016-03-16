5ed430ad7120fb3cf61e947180cc50359650770a
Tao <taoluo@combitel.com.au>
2016-03-16 14:52:09 +1100

    update order list column

--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from django.core.urlresolvers import reverse
 from rest_framework import serializers
-from models import Order, ORDER_STATUS
+from models import Order, ORDER_STATUS, ORDER_STATUS_CHOICES
 
 
 # Serializer for order
@@ -12,13 +12,14 @@ class OrderSerializer(serializers.ModelSerializer):
     customer_url = serializers.SerializerMethodField()
     shipping_order = serializers.SerializerMethodField()
     paid_url = serializers.SerializerMethodField()
+    public_link = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['link'] + ['customer', 'customer_display', 'address', 'is_paid', 'status', 'total_amount',
-                             'product_cost_aud', 'customer_url', 'shipping_order', 'status_button', 'paid_url',
-                             'product_cost_rmb', 'shipping_fee', 'ship_time', 'total_cost_aud', 'total_cost_rmb',
-                             'origin_sell_rmb', 'sell_price_rmb', 'profit_rmb', 'create_time', 'finish_time'] + ['id']
+        fields = ['link', 'customer', 'customer_display', 'address', 'is_paid', 'status', 'total_amount', 'public_link',
+                  'product_cost_aud', 'customer_url', 'shipping_order', 'status_button', 'paid_url',
+                  'product_cost_rmb', 'shipping_fee', 'ship_time', 'total_cost_aud', 'total_cost_rmb',
+                  'origin_sell_rmb', 'sell_price_rmb', 'profit_rmb', 'create_time', 'finish_time', 'id']
         read_only_fields = ['id']
 
     def get_link(self, obj):
@@ -44,18 +45,23 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_status_button(self, obj):
         current_status = ''
         if obj.status in [ORDER_STATUS.CREATED, ORDER_STATUS.DELIVERED]:
-            current_status += '<b>%s</b>' % obj.status
+            current_status += '<b>%s</b>' % obj.get_status_display()
         else:
-            current_status += obj.status
+            current_status += obj.get_status_display()
 
         next_status = obj.next_status
         if not next_status:
-            return ORDER_STATUS.FINISHED
+            return u'完成'
 
-        btn = '%s<br/> => <a href="%s">%s</a>' % (current_status, url, next_status)
+        next_status_text = dict(ORDER_STATUS_CHOICES)[next_status]
+
         url = reverse('order:change-order-status', kwargs={'order_id': obj.id, 'status_value': next_status})
+        btn = '%s > <a href="%s">%s</a>' % (current_status, url, next_status_text)
         return btn
 
     def get_paid_url(self, obj):
         url = reverse('order:change-order-paid', args=[obj.id])
         return url
+
+    def get_public_link(self, obj):
+        return obj.get_id_link()
