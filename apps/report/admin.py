9100b0cbda7a4734f7f1770675b173ce038e3fd7
Tao <taoluo@combitel.com.au>
2016-03-17 12:47:25 +1100

    adjust admin list field for monthly report

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import MonthlyReport
-# Register your models here.
 
-admin.site.register(MonthlyReport)
+
+class MonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ('month', 'cost_aud', 'cost_rmb', 'shipping_fee', 'sell_price_rmb', 'profit_rmb')
+    list_filter = ['month']
+    ordering = ['-month']
+    list_display_links = list_display
+
+
+admin.site.register(MonthlyReport, MonthlyReportAdmin)
