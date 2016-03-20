1fb5351f9c6e8331d680c489f5a2c3daab6fb9b8
Tao <www@luotao.net>
2016-03-20 11:21:47 +0800

    add migrations

---
+++ apps/order/migrations/0009_auto_20160320_1421.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('order', '0008_auto_20160316_2152'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='order',
+            name='paid_time',
+            field=models.DateTimeField(null=True, verbose_name='Paid Time', blank=True),
+        ),
+    ]
