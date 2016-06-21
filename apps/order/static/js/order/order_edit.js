9ece2b06443c0ee259edcdb70848b4a94a4b13bc
Tao <taoluo@combitel.com.au>
2016-06-21 15:49:08 +1000

    finish express order deleting

--- apps/order/static/js/order/order_edit.js
+++ apps/order/static/js/order/order_edit.js
@@ -24,11 +24,11 @@ var OrderEditPageVue = Vue.extend({
 
     },
     methods: {
-        addone: function (event) {
+        add_product: function (event) {
             var order_pk = $("input#order_pk").val();
 
-            var $TOTAL_FORMS = $("input#id_form-TOTAL_FORMS");
-            $("input#id_form-" + ($TOTAL_FORMS.val() - 1) + "-order").val(order_pk);
+            var $TOTAL_FORMS = $("#product_table input#id_form-TOTAL_FORMS");
+            $("#product_table input#id_form-" + ($TOTAL_FORMS.val() - 1) + "-order").val(order_pk);
 
             var $product_template = $("#product_template");
             var $product_template_copy = $product_template.children().clone(false);
@@ -68,6 +68,44 @@ var OrderEditPageVue = Vue.extend({
             $("select[class$='form-control']").not(".hide select[class$='form-control']")
                 .chosen({search_contains: true, disable_search_threshold: 10});
         },
+        add_express: function (event) {
+            var order_pk = $("input#order_pk").val();
+
+            var $TOTAL_FORMS = $("#express_table input#id_form-TOTAL_FORMS");
+            $("#express_table input#id_form-" + ($TOTAL_FORMS.val() - 1) + "-order").val(order_pk);
+
+            var $express_template = $("#express_template");
+            var $express_template_copy = $express_template.children().clone(false);
+            var base_id = "id_form-" + $TOTAL_FORMS.val();
+            var base_name = "form-" + $TOTAL_FORMS.val();
+
+            $("input#id_order", $express_template_copy).val(order_pk);
+            $("input#id_order", $express_template_copy).attr("name", base_name + "-order");
+            $("input#id_order", $express_template_copy).attr("id", base_id + "-order");
+
+            $("select#id_carrier", $express_template_copy).attr("name", base_name + "-carrier");
+            $("select#id_carrier", $express_template_copy).attr("id", base_id + "-carrier");
+
+            $("input#id_track_id", $express_template_copy).attr("name", base_name + "-track_id");
+            $("input#id_track_id", $express_template_copy).attr("id", base_id + "-track_id");
+
+            $("input#id_fee", $express_template_copy).attr("name", base_name + "-fee");
+            $("input#id_fee", $express_template_copy).attr("id", base_id + "-fee");
+
+            $("input#id_weight", $express_template_copy).attr("name", base_name + "-weight");
+            $("input#id_weight", $express_template_copy).attr("id", base_id + "-weight");
+
+            $("input#id_id_upload", $express_template_copy).attr("name", base_name + "-id_upload");
+            $("input#id_id_upload", $express_template_copy).attr("id", base_id + "-id_upload");
+
+            $express_template_copy.attr('id', 'form-' + $TOTAL_FORMS.val());
+            var $element = $("#express_table").append($express_template_copy);
+            this.$compile($element.get(0)); // link event for delete button
+            $TOTAL_FORMS.val(parseInt($TOTAL_FORMS.val()) + 1);
+
+            $("select[class$='form-control']").not(".hide select[class$='form-control']")
+                .chosen({search_contains: true, disable_search_threshold: 10});
+        },
         submit: function (event) {
             var total_forms = parseInt($("input#id_form-TOTAL_FORMS").val());
             var order_pk = $("input#order_pk").val();
@@ -88,8 +126,9 @@ var OrderEditPageVue = Vue.extend({
             document.getElementById("commonForm").submit();
         },
         delete_product: function (event) {
-            var $TOTAL_FORMS = $("input#id_form-TOTAL_FORMS");
+            var $TOTAL_FORMS = $("#product_table input#id_form-TOTAL_FORMS");
             var product = $(event.target).closest('div.form-group');
+            console.log(product[0]);
             var orderProductID = $("input:hidden[id$='-id']", product).val();
             if (orderProductID) {
                 swal({
@@ -115,6 +154,39 @@ var OrderEditPageVue = Vue.extend({
                 });
             } else {
                 product.remove();
+                $TOTAL_FORMS.val(parseInt($TOTAL_FORMS.val()) - 1);
+            }
+        },
+        delete_express: function (event) {
+            var $TOTAL_FORMS = $("#express_table input#id_form-TOTAL_FORMS");
+            var express = $(event.target).closest('div.form-group');
+            console.log(express[0]);
+            var orderExpressID = $("input:hidden[id$='-id']", express).val();
+            console.log(orderExpressID);
+            if (orderExpressID) {
+                swal({
+                    title: "确定删除",
+                    text: "确定删除所选快递信息?",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    closeOnConfirm: true,
+                    showLoaderOnConfirm: false
+                }, function () {
+                    var deleteUrl = Urls['express:api-expressorder-delete']();
+                    $.AdminLTE.apiDelete(
+                        deleteUrl,
+                        $.param({'pk': orderExpressID}),
+                        function (resp) {
+                            express.remove();
+                            $TOTAL_FORMS.val(parseInt($TOTAL_FORMS.val()) - 1);
+                        }
+                    );
+                });
+            } else {
+                express.remove();
                 $TOTAL_FORMS.val(parseInt($TOTAL_FORMS.val()) - 1);
             }
         }
