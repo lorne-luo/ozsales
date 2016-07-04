e1e48c7cb86fc92d085d9446d6bd9bb15f9d25f1
Tao <taoluo@combitel.com.au>
2016-07-04 16:09:50 +1000

    add default for carrier

--- apps/express/models.py
+++ apps/express/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.core.urlresolvers import reverse
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 
 from ..order.models import Order
 from ..customer.models import Address
@@ -11,11 +11,12 @@ from ..customer.models import Address
 
 @python_2_unicode_compatible
 class ExpressCarrier(models.Model):
-    name_cn = models.CharField(_(u'name_cn'), max_length=50, null=False, blank=False)
-    name_en = models.CharField(_(u'name_en'), max_length=50, null=True, blank=True)
-    website = models.URLField(_(u'website'), blank=True, null=True)
-    search_url = models.URLField(_(u'search url'), blank=True, null=True)
-    rate = models.DecimalField(_(u'rate'), max_digits=6, decimal_places=2, blank=True, null=True)
+    name_cn = models.CharField(_('name_cn'), max_length=50, null=False, blank=False)
+    name_en = models.CharField(_('name_en'), max_length=50, null=True, blank=True)
+    website = models.URLField(_('Website'), blank=True, null=True)
+    search_url = models.URLField(_('Search url'), blank=True, null=True)
+    rate = models.DecimalField(_('Rate'), max_digits=6, decimal_places=2, blank=True, null=True)
+    is_default = models.BooleanField('Default', default=False)
 
     class Meta:
         verbose_name_plural = _('Express Carrier')
@@ -27,7 +28,7 @@ class ExpressCarrier(models.Model):
 
 @python_2_unicode_compatible
 class ExpressOrder(models.Model):
-    carrier = models.ForeignKey(ExpressCarrier, blank=False, null=False, verbose_name=_('carrier'))
+    carrier = models.ForeignKey(ExpressCarrier, blank=True, null=True, verbose_name=_('carrier'))
     track_id = models.CharField(_('Track ID'), max_length=30, null=False, blank=False)
     order = models.ForeignKey(Order, blank=False, null=False, verbose_name=_('order'), related_name='express_orders')
     address = models.ForeignKey(Address, blank=True, null=True, verbose_name=_('address'))
@@ -47,6 +48,9 @@ class ExpressOrder(models.Model):
         return '[%s]%s' % (self.carrier.name_cn, self.track_id)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.carrier:
+            self.carrier = ExpressCarrier.objects.filter(is_default=True).first()
+
         if not self.address and self.order and self.order.address:
             self.address = self.order.address
         return super(ExpressOrder, self).save()
@@ -82,3 +86,9 @@ class ExpressOrder(models.Model):
 def update_order_price(sender, instance=None, created=False, **kwargs):
     if instance.order and instance.order.id:
         instance.order.update_price()
+
+
+@receiver(pre_save, sender=ExpressCarrier)
+def update_default_carrier(sender, instance=None, created=False, **kwargs):
+    if instance.is_default:
+        ExpressCarrier.objects.all().update(is_default=False)
