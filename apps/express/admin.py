6686068d0791857791cb547444f582c19e9f4557
Tao <taoluo@combitel.com.au>
2016-07-05 12:44:31 +1000

    change search and filter for express order

--- apps/express/admin.py
+++ apps/express/admin.py
@@ -8,7 +8,8 @@ class ExpressOrderAdmin(admin.ModelAdmin):
     list_display = ('carrier', 'get_tracking_link', 'get_order_link', 'address', 'fee', 'weight', 'create_time')
     ordering = ['-create_time']
     list_display_links = ['address', 'carrier']
-    search_fields = ('order__customer__name', 'address__name', 'track_id', 'carrier_name_cn', 'carrier_name_en')
+    search_fields = ('order__customer__name', 'address__name', 'address__address', 'track_id')
+    filter_fields = ('carrier__name_cn')
 
 
 admin.site.register(ExpressOrder, ExpressOrderAdmin)
