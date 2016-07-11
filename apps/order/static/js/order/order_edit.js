fd6fe83f47784ff0c25d8d4aecf6d87fea2a4204
Tao <taoluo@combitel.com.au>
2016-07-11 14:31:34 +1000

    fix express add and update

--- apps/order/static/js/order/order_edit.js
+++ apps/order/static/js/order/order_edit.js
@@ -71,43 +71,37 @@ var OrderEditPageVue = Vue.extend({
         },
         add_express: function (event) {
             var order_pk = $("input#order_pk").val();
+            var form_name = "express_orders";
+            var base = "id_" + form_name;
 
-            var $TOTAL_FORMS = $("#express_table input#id_express_orders-TOTAL_FORMS");
-            $("#express_table input#id_express_orders-" + ($TOTAL_FORMS.val() - 1) + "-order").val(order_pk);
+            var $TOTAL_FORMS = $("#express_table input#" + base + "-TOTAL_FORMS");
+            // $("#express_table input#id_express_orders-" + ($TOTAL_FORMS.val() - 1) + "-order").val(order_pk);
 
             var $express_template = $("#express_template");
             var $express_template_copy = $express_template.children().clone(false);
             console.log($express_template_copy[0]);
-            var base_id = "id_express_orders-" + $TOTAL_FORMS.val();
+            var base_id = base + "-" + $TOTAL_FORMS.val();
             var base_name = "express_orders-" + $TOTAL_FORMS.val();
 
-            $("input#id_order", $express_template_copy).val(order_pk);
-            $("input#id_order", $express_template_copy).attr("name", base_name + "-order");
-            $("input#id_order", $express_template_copy).attr("id", base_id + "-order");
+            var fields = ["order", "carrier", "track_id", "fee", "weight", "id_upload"];
+            for (var i in fields) {
+                var field = fields[i];
+                console.log("#" + base + "-" + field);
+                var input = $("#" + base + "-" + field, $express_template_copy);
+                console.log(input.get(0));
+                input.attr("name", base_name + "-" + field);
+                input.attr("id", base_id + "-" + field);
+            }
+            $("#" + base_id + "-order", $express_template_copy).val(order_pk); //set up order_id
 
-            $("select#id_carrier", $express_template_copy).attr("name", base_name + "-carrier");
-            $("select#id_carrier", $express_template_copy).attr("id", base_id + "-carrier");
-
-            $("input#id_track_id", $express_template_copy).attr("name", base_name + "-track_id");
-            $("input#id_track_id", $express_template_copy).attr("id", base_id + "-track_id");
-
-            $("input#id_fee", $express_template_copy).attr("name", base_name + "-fee");
-            $("input#id_fee", $express_template_copy).attr("id", base_id + "-fee");
-
-            $("input#id_weight", $express_template_copy).attr("name", base_name + "-weight");
-            $("input#id_weight", $express_template_copy).attr("id", base_id + "-weight");
-
-            $("input#id_id_upload", $express_template_copy).attr("name", base_name + "-id_upload");
-            $("input#id_id_upload", $express_template_copy).attr("id", base_id + "-id_upload");
-
-            $express_template_copy.attr('id', 'express_orders-' + $TOTAL_FORMS.val());
+            $express_template_copy.attr('id', base_name);
             var $element = $("#express_table").append($express_template_copy);
             console.log($element.get(0));
             this.$compile($element.get(0)); // link event for delete button
             $TOTAL_FORMS.val(parseInt($TOTAL_FORMS.val()) + 1);
 
             $("select[class$='form-control']").not(".hide select[class$='form-control']")
-                .chosen({search_contains: true, disable_search_threshold: 10});
+              .chosen({search_contains: true, disable_search_threshold: 10});
         },
         submit: function (event) {
             // var total_forms = parseInt($("input#id_form-TOTAL_FORMS").val());
