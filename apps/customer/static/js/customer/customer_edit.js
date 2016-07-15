2d83e877b011be83eeb5dce5e77638c163180a47
Tao <taoluo@combitel.com.au>
2016-07-15 16:09:05 +1000

    add method to fill related fk

--- apps/customer/static/js/customer/customer_edit.js
+++ apps/customer/static/js/customer/customer_edit.js
@@ -1,23 +1,24 @@
 var CustomerEditPageVue = CommonFormPageVue.extend({
-  el: function () {
-    return 'form#commonForm';
-  },
-  data: function () {
-    return {
-
+    el: function () {
+        return 'form#commonForm';
+    },
+    data: function () {
+        return {}
+    },
+    methods: {
+        add_address: function (event) {
+            console.log('123');
+            var form_name = "address_set";
+            var fields = ["customer", "name", "mobile", "address", "id_number", "id_photo_front", "id_photo_back"];
+            this.add_item(form_name, fields);
+        },
+        delete_address: function (event) {
+            var url_tag = 'customer:api-address-delete';
+            var fields = ["id", "customer", "name", "mobile", "address", "id_number", "id_photo_front", "id_photo_back"];
+            this.delete_item(event, url_tag, fields);
+        },
+        submit: function (event) {
+            this.fill_related('address_set','name','customer');
+        }
     }
-  },
-  methods: {
-    add_address: function (event) {
-      console.log('123');
-      var form_name = "address_set";
-      var fields = ["customer", "name", "mobile", "address", "id_number", "id_photo_front", "id_photo_back"];
-      this.add_item(form_name, fields);
-    },
-    delete_address: function (event) {
-      var url_tag = 'customer:api-address-delete';
-      var fields = ["id", "customer",  "name", "mobile", "address", "id_number", "id_photo_front", "id_photo_back"];
-      this.delete_item(event, url_tag, fields);
-    },
-  }
 });
