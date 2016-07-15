64d244ac646054e07a3b1d6f993d48d00b78e860
Tao <taoluo@combitel.com.au>
2016-07-15 12:56:29 +1000

    add placeholder for form input

--- core/libs/forms.py
+++ core/libs/forms.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,11 +7,21 @@ class ModelForm(forms.ModelForm):
     """
     remove the Hold down hint for manytomanyfield
     """
+
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
         remove_message = unicode(_('Hold down "Control", or "Command" on a Mac, to select more than one.'))
-        for field in self.base_fields:
-            if self.base_fields[field].help_text:
-                help_text=unicode(self.base_fields[field].help_text)
+        for field_name in self.base_fields:
+            if self.base_fields[field_name].help_text:
+                help_text = unicode(self.base_fields[field_name].help_text)
                 help_text = help_text.replace(remove_message, '').strip()
-                self.fields[field].help_text = help_text
+                self.fields[field_name].help_text = help_text
+
+            field = self.fields.get(field_name)
+            if type(field.widget) in (forms.TextInput, forms.DateInput, forms.EmailInput):
+                if field.required:
+                    field.widget.attrs['placeholder'] = u'必填'
+                else:
+                    field.widget.attrs['placeholder'] = u'选填'
+            elif type(field.widget) in [forms.SelectMultiple]:
+                field.widget.attrs['placeholder'] = u'可多选'
