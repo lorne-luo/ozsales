8d7f3a266d036d18408a49ba154b46e71d0eb843
Tao <taoluo@combitel.com.au>
2016-09-01 14:05:19 +1000

    add deploy log

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -9,4 +9,7 @@ git --git-dir=$PROJECT_ROOT/.git/ pull
 $PILOT_PYTHON $PROJECT_ROOT/manage.py collectstatic_js_reverse
 $PILOT_PYTHON $PROJECT_ROOT/manage.py collectstatic --noinput
 
+COMMIT_INFO=`git --git-dir=$PROJECT_ROOT/.git/ show -s --date=iso8601 --format='{"commit": "%h", "date": "%ad", "comment": "%s"}'`
+echo [`date +%Y-%m-%d\ %H:%M:%S`] $COMMIT_INFO >> $PROJECT_ROOT/deploy/upgrade.log
+
 exec "$@"
