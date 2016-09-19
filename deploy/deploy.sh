538ead09746afbcc9a3cbed9c693a82c7c13ab9f
Tao <taoluo@combitel.com.au>
2016-09-19 15:37:23 +1000

    update task,deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,6 +15,7 @@ else
     # Code has been updated, rebuild the app
 
     PILOT_PYTHON=$WORKDIR/env/bin/python
+    $PILOT_PYTHON manage.py migrate
     $PILOT_PYTHON manage.py collectstatic_js_reverse
     $PILOT_PYTHON manage.py collectstatic --noinput
     COMMIT_INFO=`git show -s --date=iso8601 --format='{"commit": "%h", "date": "%ad", "comment": "%s"}'`
--- settings/celery.py
+++ settings/celery.py
@@ -19,7 +19,7 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-@periodic_task(run_every=crontab(minute=0, hour='12,20'))
+@periodic_task(run_every=crontab(minute=0, hour='8,12,16,20',day_of_week='mon,tue,wed,thu,fri'))
 def get_aud_rmb():
     # url = 'http://download.finance.yahoo.com/d/quotes.csv?s=AUDCNY=X&f=sl1d1t1ba&e=.csv'
     audcny = Currency('AUDCNY')
