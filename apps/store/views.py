cf4f0f40b423ddeb1f0d291b3fc458294a98426f
Tao <taoluo@combitel.com.au>
2016-10-20 16:11:02 +1100

    relocate common views

--- apps/store/views.py
+++ apps/store/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from braces.views import MultiplePermissionsRequiredMixin, PermissionRequiredMixin
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
-from core.adminlte.views import CommonContextMixin, CommonViewSet
+from core.views.views import CommonContextMixin, CommonViewSet
 from models import Page, Store
 import serializers
 import forms
