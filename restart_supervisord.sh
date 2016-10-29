44a463748811884afbe83806ec1fe10667482e63
Tao <www@luotao.net>
2016-10-29 12:15:04 +0800

    comment celery restart

--- restart_supervisord.sh
+++ restart_supervisord.sh
@@ -2,4 +2,4 @@ env/bin/python manage.py collectstatic_js_reverse
 env/bin/python manage.py collectstatic --noinput
 
 supervisorctl restart ozsales
-supervisorctl restart celery
+#supervisorctl restart celery
