16c25b92626d4794cc476e1dd6b935f8fcf86626
Tao <git@luotao.net>
2017-02-09 23:37:40 +1100

    create sms model

---
+++ core/sms/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Sms',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('time', models.DateTimeField(auto_now_add=True, verbose_name='DateTime')),
+                ('app_name', models.CharField(max_length=16, null=True, verbose_name='App Name', blank=True)),
+                ('send_to', models.CharField(max_length=32, verbose_name='Send To')),
+                ('content', models.CharField(max_length=255, null=True, verbose_name='Content', blank=True)),
+                ('url', models.URLField(max_length=255, null=True, verbose_name='Url', blank=True)),
+                ('success', models.BooleanField(default=False, verbose_name='Success')),
+            ],
+        ),
+    ]
--- settings/settings.py
+++ settings/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = (
     'apps.schedule',
     'apps.registration',
     'apps.weixin',
+    'core.sms',
     'utils',
 
     # third_app
