c0a0a5bf1fbbf8d4b134347b63ec773d0fe3b333
Tao <git@luotao.net>
2017-08-17 23:41:39 +1000

    add ozbargin task

---
+++ apps/schedule/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OzbarginTask',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('includes', models.CharField(max_length=255, verbose_name='includes')),
+                ('excludes', models.CharField(max_length=255, verbose_name='excludes')),
+                ('is_active', models.BooleanField(default=True, verbose_name='is_active')),
+                ('create_at', models.DateTimeField(auto_now_add=True, verbose_name='Create at', null=True)),
+            ],
+        ),
+    ]
--- apps/schedule/models.py
+++ apps/schedule/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class OzbarginTask(models.Model):
+    includes = models.CharField(_(u'includes'), max_length=255, null=False, blank=False)
+    excludes = models.CharField(_(u'excludes'), max_length=255, null=False, blank=False)
+    is_active = models.BooleanField(_(u'is_active'), default=True, blank=False, null=False)
+    create_at = models.DateTimeField(_('Create at'), auto_now_add=True, null=True)
