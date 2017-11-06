d68638008f8ed31b204af5dcad59fa976c1c6f41
Lorne <git@luotao.net>
2017-11-07 01:50:42 +1100

    introduce django material

--- settings/settings.py
+++ settings/settings.py
@@ -30,6 +30,9 @@ INTERNAL_IPS = ('0.0.0.0', '127.0.0.1')
 
 # Application definition
 INSTALLED_APPS = (
+    'material',
+    'material.frontend',
+    # 'material.admin',
     'dal',
     'dal_select2',
     'django.contrib.admin',
