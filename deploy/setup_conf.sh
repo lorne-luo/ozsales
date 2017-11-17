cd8b0c9dae46014672a1e83ec838e4a5df2d9c22
Lorne <lorne.luo@butterfly.com.au>
2017-11-17 13:39:47 +1100

    add sctripe to link conf

---
+++ deploy/setup_conf.sh
@@ -0,0 +1,3 @@
+ln -s nginx_ozsale_uwsgi.conf  /usr/local/nginx/conf/vhost/ozsales.conf
+ln -s nginx_supervisord.conf /usr/local/nginx/conf/vhost/supervisord.conf
+ln -s supervisord_ozsales.conf /etc/supervisor/supervisor.d/ozsales.conf
