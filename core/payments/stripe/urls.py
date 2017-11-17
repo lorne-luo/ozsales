d32b080eb52e273ab9b2805ec190c747032dcff0
Lorne <lorne.luo@butterfly.com.au>
2017-11-17 13:40:01 +1100

    add web hook for stripe

--- core/payments/stripe/urls.py
+++ core/payments/stripe/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from djstripe.views import WebHook
 import views
 
 urlpatterns = [
     url(r'^payments/add_card/$', views.UpdateCreditCardView.as_view(), name='add_card'),
+    url(r"^payments/webhook/$", WebHook.as_view(), name="webhook"),
 ]
