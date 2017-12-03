7efd7023fc04f09921cc1dc742b02620f90ab3a6
Lorne <dev@luotao.net>
2017-12-03 21:29:17 +1100

    default serializer for express app

--- apps/express/serializers.py
+++ apps/express/serializers.py
@@ -5,18 +5,16 @@ from models import ExpressCarrier, ExpressOrder
 
 
 # Serializer for expresscarrier
-class ExpressCarrierSerializer(BaseSerializer):
+class ExpressCarrierSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpressCarrier
-        fields = ['id', 'edit_url', 'detail_url'] + \
-                 ['name_cn', 'name_en', 'website', 'search_url', 'id_upload_url', 'track_id_regex', 'is_default']
+        fields = ['id', 'name_cn', 'name_en', 'website', 'search_url', 'id_upload_url', 'track_id_regex', 'is_default']
         read_only_fields = ['id']
 
 
 # Serializer for expressorder
-class ExpressOrderSerializer(BaseSerializer):
+class ExpressOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpressOrder
-        fields = ['id', 'edit_url', 'detail_url'] + \
-                 ['carrier', 'track_id', 'order', 'address', 'fee', 'weight', 'id_upload', 'remarks']
+        fields = ['id', 'carrier', 'track_id', 'order', 'address', 'fee', 'weight', 'id_upload', 'remarks']
         read_only_fields = ['id']
