2a680abcdd7df071ad6ba0d2aa07cdc263f4984c
Lorne <lorne.luo@butterfly.com.au>
2018-01-12 14:07:27 +1100

    fix bug in code

--- core/autocode/management/commands/templates/api_views.py
+++ core/autocode/management/commands/templates/api_views.py
@@ -1,7 +1,7 @@
 API_VIEWS_HEADER = '''# coding=utf-8
 from rest_framework import permissions
 from core.api.views import CommonViewSet
-from ..models import <% MODEL_NAME %>
+from ..models import <% ALL_MODELS %>
 from . import serializers
 
 '''
