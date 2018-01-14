e4c3cd36834900056c9179986ac27acf80858e5a
Lorne <dev@luotao.net>
2018-01-14 17:25:20 +1100

    refactor js for order edit

--- apps/order/static/js/order/order_edit.js
+++ apps/order/static/js/order/order_edit.js
@@ -1,13 +1,30 @@
+var OrderEditPageVue = CommonFormPageVue.extend({
+    ready: function () {
+        $('input').not('[id$="-DELETE"]').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
 
-$(document).ready(function () {
-    $('input').not('[id$="-DELETE"]').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' // optional
-    });
+        $("#address-text").text($("#id_address option:selected").text());
+        $("#id_address").change(function () {
+            $("#address-text").text($("option:selected", this).text());
+        });
+    }
+});
 
-    $("#address-text").text($("#id_address option:selected").text());
-    $("#id_address").change(function () {
-        $("#address-text").text($("option:selected", this).text());
-    });
+var orderEditPageVue = new OrderEditPageVue({
+    data: {
+        // API
+        list_api_tag: 'api:order-list',
+        detail_api_tag: 'api:order-detail',
+        product_detail_api_tag: 'api:orderproduct-detail',
+        delete_api_tag: 'api:order-delete',
+        // page
+        create_url_tag: 'order:order-add',
+        list_url_tag: 'order:order-list-short',
+        update_url_tag: 'order:order-update',
+        detail_url_tag: 'order-detail-short',
+        list_url: Urls['api:order-list']()
+    }
 });
