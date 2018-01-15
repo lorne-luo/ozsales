d6b243a266a286986ad7dc839903915eeeb3b5f1
Lorne <lorne.luo@butterfly.com.au>
2018-01-16 09:58:02 +1100

    update schedule of update track task

--- apps/express/tasks.py
+++ apps/express/tasks.py
@@ -7,7 +7,7 @@ from apps.order.models import Order, ORDER_STATUS
 log = logging.getLogger(__name__)
 
 
-@periodic_task(run_every=crontab(minute='*/240', hour='7-0'))
+@periodic_task(run_every=crontab(minute=0, hour='9,12,15,18,21,24'))
 def update_delivery_tracking():
     for order in Order.objects.filter(status__in=[ORDER_STATUS.SHIPPING, ORDER_STATUS.CREATED]):
         if not order.seller.check_premium_member():
