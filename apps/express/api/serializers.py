896c9e786b327893af5ddf9fe8d900ea62d0a100
Lorne <dev@luotao.net>
2018-01-21 13:42:44 +1100

    carrier list permission, edit and delete for owner only

--- apps/express/api/serializers.py
+++ apps/express/api/serializers.py
@@ -1,15 +1,17 @@
 # coding=utf-8
 from rest_framework import serializers
-from core.api.serializers import BaseSerializer
+from core.api.serializers import BaseSerializer, SellerOwnerSerializerMixin
 from ..models import ExpressCarrier, ExpressOrder
 
 
 # Serializer for expresscarrier
-class ExpressCarrierSerializer(BaseSerializer):
+class ExpressCarrierSerializer(SellerOwnerSerializerMixin,BaseSerializer):
+    is_owner = serializers.SerializerMethodField()
+
     class Meta:
         model = ExpressCarrier
-        fields = ['id', 'edit_url', 'detail_url'] + \
-                 ['name_cn', 'name_en', 'website', 'search_url', 'id_upload_url', 'track_id_regex', 'is_default']
+        fields = ['id', 'edit_url', 'detail_url', 'is_owner', 'name_cn', 'name_en', 'website', 'search_url',
+                  'id_upload_url', 'track_id_regex', 'is_default']
         read_only_fields = ['id']
 
 
