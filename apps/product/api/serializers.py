2acd960209498d61c519bcf55c730c52dbfc9e8f
Lorne <dev@luotao.net>
2018-06-11 17:46:13 +1000

    change str into unicode

--- apps/product/api/serializers.py
+++ apps/product/api/serializers.py
@@ -20,7 +20,7 @@ class ProductSerializer(SellerOwnerSerializerMixin, BaseSerializer):
         return obj.pic.thumbnail.url if obj.pic else None
 
     def get_brand_display(self, obj):
-        return str(obj.brand)
+        return unicode(obj.brand)
 
 
 class BrandSerializer(BaseSerializer):
