fc8ca935c6082b59956373ddd3d3f2727226edce
Lorne <lorne.luo@butterfly.com.au>
2018-09-27 15:56:38 +1000

    refine express track task

--- apps/carrier_tracker/tasks.py
+++ apps/carrier_tracker/tasks.py
@@ -11,27 +11,31 @@ log = logging.getLogger(__name__)
 # @periodic_task(run_every=crontab(minute=7, hour='9,12,15,18,21,0'))
 @task
 def update_delivery_tracking():
-    for order in Order.objects.filter(status=ORDER_STATUS.SHIPPING):
-        if not order.seller.is_premium:
+    for tenant in Tenant.objects.normal():
+        if not tenant.seller or not tenant.seller.is_premium:
             continue
-        order.update_track()
+
+        tenant.set_schema()
+        for order in Order.objects.filter(status=ORDER_STATUS.SHIPPING):
+            if not order.seller.is_premium:
+                continue
+            order.update_track()
 
 
 # @periodic_task(run_every=crontab(minute=5, hour='11,14,17,19,23'))
 @task
 def send_delivery_sms():
     for tenant in Tenant.objects.normal():
+        if not tenant.seller or not tenant.seller.is_premium:
+            continue
+
         tenant.set_schema()
         for order in Order.objects.filter(status=ORDER_STATUS.DELIVERED):
-            if not order.seller.is_premium:
-                continue
             if not order.delivery_msg_sent:
                 order.sms_delivered()
             order.set_status(ORDER_STATUS.FINISHED)
 
         for order in Order.objects.filter(status=ORDER_STATUS.SHIPPING):
-            if not order.seller.is_premium:
-                continue
             order.sms_delivered()
             if order.is_all_delivered:
                 order.set_status(ORDER_STATUS.FINISHED)
