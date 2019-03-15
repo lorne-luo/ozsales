4d21f7ba63eb8fdff61f7b3d62e7f1cce34ac9f3
Lorne <lorne.luo@butterfly.com.au>
2019-03-15 14:21:35 +1100

    move venv folder into luotao

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -1,1 +1,1 @@
-/home/git/venv/ozsales/bin/python /opt/ozsales/manage.py dumpdata --indent 4 | gzip -c > /opt/backup/ozsales/`date +%d`.json.gz
+/home/luotao/venv/ozsales/bin/python /opt/ozsales/manage.py dumpdata --indent 4 | gzip -c > /opt/backup/ozsales/`date +%d`.json.gz
